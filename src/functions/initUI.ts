106b4e6c70a08d406674b91811f59787ab962e35
Tharindu Nimesh <127307898+TharinduNimesh@users.noreply.github.com>
2024-05-24 17:36:43 +0530

    append ui if element is undefined

--- src/functions/initUI.ts
+++ src/functions/initUI.ts
@@ -4,14 +4,47 @@ import { UIElements } from "../types";
 export function initUI(elements: UIElements | undefined): void {
   if (elements === undefined) {
     /* 
-            Add Design to control music...
+        Add Design to control music...
     */
+    const container = document.createElement("div");
+    container.classList.add("song-controller-container");
+    container.innerHTML = `
+        <div class="controller-container">
+          <div class="corner"></div>
+          <div class="controllers">
+            <i class="fa-solid fa-backward" id="prev-button"></i>
+            <i class="fa-solid fa-play" id="play-pause-button"></i>
+            <i class="fa-solid fa-forward" id="next-button"></i>
+          </div>
+        </div>
+        <i class="fa-solid fa-music"></i>
+    `;
+    document.body.appendChild(container);
 
-    return;
+    // Get the elements
+    const playPauseButton = document.getElementById("play-pause-button");
+    const nextButton = document.getElementById("next-button");
+    const prevButton = document.getElementById("prev-button");
+
+    if (
+      playPauseButton === null ||
+      nextButton === null ||
+      prevButton === null
+    ) {
+      console.error("Elements not found");
+      return;
+    }
+
+    // Set the elements
+    elements = {
+      playPauseButton,
+      nextButton,
+      prevButton,
+    };
   }
 
   /* 
-        Call controller functions from given Elements...  
+      Call controller functions from given Elements...  
   */
   elements.playPauseButton.addEventListener("click", () => {
     // Play or Pause the music
@@ -27,6 +60,4 @@ export function initUI(elements: UIElements | undefined): void {
     // Play previous song
     MusicJS.prev();
   });
-
-  return;
 }
