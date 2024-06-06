afd56b5fe7438f0b3b85e3ea0f88c976f9fe100d
Tharindu Nimesh <127307898+TharinduNimesh@users.noreply.github.com>
2024-06-06 19:58:22 +0530

    File base audio controls

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,22 @@
-import { InitOptions, Method } from "./types";
+import { InitOptions } from "./types";
 import * as YT from "./functions/youtube";
+import * as FILE from "./functions/file";
 import { YouTubePlayer as YouTubePlayerType } from "youtube-player/dist/types";
 import { initUI, shuffle } from "./functions";
 
 export default class MusicJS {
+  static options: InitOptions | null = null;
+
   static YTPlayer: YouTubePlayerType | null = null;
+  static AudioPlayer: HTMLAudioElement | null = null;
+
   static songs: string[] = [];
   static currentSongIndex: number = 0;
 
   static init(options: InitOptions): void {
+    // Save options for later use
+    this.options = options;
+
     // Initialize UI Controllers for to control Music
     initUI(options.elements);
 
@@ -17,16 +25,9 @@ export default class MusicJS {
       this.YTPlayer = YT.init();
 
       // Setup songs
-      if (!Array.isArray(options.src)) {
-        // extract song ID from URL & push it into this.songs
-        const songId = YT.getIdByUrl(options.src);
-        this.songs.push(songId);
-      } else {
-        this.setSongs(options.src, options.method);
-        // Shuffle songs if randomize option is enabled
-        if (options.randomize) {
-          this.songs = shuffle<string>(this.songs);
-        }
+      this.setSongs(options.src);
+      if (options.randomize) {
+        this.songs = shuffle<string>(this.songs);
       }
 
       // Start playing first song
@@ -43,27 +44,67 @@ export default class MusicJS {
           this.next();
         }
       });
-    }
-  }
-
-  static setSongs(songs: string[], method: Method): void {
-    // set songs' IDs into this.songs if type is YT
-    if (method === "youtube") {
-      songs.forEach((song: string) => {
-        let url = YT.getIdByUrl(song);
-        if (url != "") {
-          this.songs.push(url);
-        }
-      });
       return;
     }
 
-    // if type is file add path directly
-    this.songs = songs;
+    // initialize file player
+    this.AudioPlayer = FILE.init();
+    this.setSongs(options.src);
+
+    // shuffle songs if randomize is true
+    if (options.randomize) {
+      this.songs = shuffle<string>(this.songs);
+    }
+
+    // Start playing first song
+    this.AudioPlayer.src = this.songs[0];
+    this.AudioPlayer.play().then(() => {
+      console.log(
+        "Starting to play song. It will take some time to buffer video before it starts playing."
+      );
+    });
+
+    // Play next song when current song ends
+    this.AudioPlayer.addEventListener("ended", () => {
+      this.next();
+    });
+  }
+
+  static setSongs(songs: string | string[]): void {
+    // set songs' IDs into this.songs if type is YT
+    if (this.options?.method === "youtube") {
+      if (Array.isArray(songs)) {
+        songs.forEach((song: string) => {
+          let url = YT.getIdByUrl(song);
+          if (url != "") {
+            this.songs.push(url);
+          }
+        });
+        return;
+      } else {
+        let url = YT.getIdByUrl(songs);
+        if (url != "") {
+          this.songs.push(url);
+          return;
+        }
+      }
+    }
+
+    // set songs' URLs into this.songs if type is file
+    if (Array.isArray(songs)) {
+      this.songs = songs;
+      return;
+    }
+
+    this.songs.push(songs);
   }
 
   static PlayOrPause(): void {
-    if (this.YTPlayer) {
+    if (this.options?.method === "youtube") {
+      if (!this.YTPlayer) {
+        return;
+      }
+      // Get current state of player and toggle it
       this.YTPlayer.getPlayerState().then((state) => {
         if (state === 1) {
           this.YTPlayer?.pauseVideo();
@@ -71,28 +112,65 @@ export default class MusicJS {
           this.YTPlayer?.playVideo();
         }
       });
+      return;
+    }
+
+    if (!this.AudioPlayer) {
+      return;
+    }
+
+    // Play or pause the audio player
+    if (this.AudioPlayer.paused) {
+      this.AudioPlayer.play();
+    } else {
+      this.AudioPlayer.pause();
     }
   }
 
   static next(): void {
+    // update currentSongIndex to next song
     if (this.currentSongIndex + 1 < this.songs.length) {
       this.currentSongIndex += 1;
     } else {
       this.currentSongIndex = 0;
     }
 
-    this.YTPlayer?.loadVideoById(this.songs[this.currentSongIndex]);
-    this.YTPlayer?.playVideo();
+    // Load and play next song based on method
+    if (this.options?.method === "youtube") {
+      this.YTPlayer?.loadVideoById(this.songs[this.currentSongIndex]);
+      this.YTPlayer?.playVideo();
+      return;
+    }
+
+    if (!this.AudioPlayer) {
+      return;
+    }
+    this.AudioPlayer.pause();
+    this.AudioPlayer.src = this.songs[this.currentSongIndex];
+    this.AudioPlayer.play();
   }
 
   static prev(): void {
+    // update currentSongIndex to previous song
     if (this.currentSongIndex - 1 >= 0) {
       this.currentSongIndex -= 1;
     } else {
       this.currentSongIndex = this.songs.length - 1;
     }
 
-    this.YTPlayer?.loadVideoById(this.songs[this.currentSongIndex]);
-    this.YTPlayer?.playVideo();
+    // Load and play previous song based on method
+    if (this.options?.method === "youtube") {
+      this.YTPlayer?.loadVideoById(this.songs[this.currentSongIndex]);
+      this.YTPlayer?.playVideo();
+      return;
+    }
+
+    if (!this.AudioPlayer) {
+      return;
+    }
+
+    this.AudioPlayer.pause();
+    this.AudioPlayer.src = this.songs[this.currentSongIndex];
+    this.AudioPlayer.play();
   }
 }
